d83118975b174bf9d19689971d3cef528d68904f
BJKerridge <bjkerridge@live.co.uk>
2025-01-08 19:41:51 +0000

    Revert Commit

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,5 +2,22 @@
 
 use BJK\Decoy\Seat\Http\Controllers\FuelController;
 
-Route::get('/fuel', [FuelController::class, 'getFuel'])
-    ->name('decoy::decoyFuel');
+Route::group([
+    'namespace'  => 'BJK\Decoy\Seat\Http\Controllers',
+    'middleware' => ['web', 'auth', 'locale'],
+], function (): void {
+
+    Route::group([
+        'prefix' => 'fuel',
+    ], function (): void {
+
+        Route::group([
+            'middleware' => 'can:global.superuser',
+        ], function (): void {
+            Route::get('/', [
+                'as' => 'decoy.fuel',
+                'uses' => 'FuelController@getFuel'
+            ]);
+        });
+    });
+});
