bf65e21da5e270b6775a24bbe92c7aa1fcdd186b
BJKerridge <bjkerridge@live.co.uk>
2025-01-08 20:21:08 +0000

    Fix Forwards

--- src/DecoyServiceProvider.php
+++ src/DecoyServiceProvider.php
@@ -61,11 +61,11 @@ class DecoyServiceProvider extends AbstractSeatPlugin
      */
     private function add_publications()
     {
-        // $this->publishes([
-        //     __DIR__ . '/resources/css' => public_path('web/css'),
-        //     __DIR__ . '/resources/img' => public_path('mumble/img'),
-        //     __DIR__ . '/resources/js' => public_path('mumble/js'),
-        // ], ['public', 'seat']);
+         $this->publishes([
+             __DIR__ . '/resources/css' => public_path('decoy/css'),
+             __DIR__ . '/resources/img' => public_path('decoy/img'),
+             __DIR__ . '/resources/js' => public_path('decoy/js'),
+         ], ['public', 'seat']);
     }
 
     /**
