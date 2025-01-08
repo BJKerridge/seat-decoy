0c2e7815b2d5ed5e33e7b3992ef5acd48a09bf5b
BJKerridge <bjkerridge@live.co.uk>
2025-01-08 01:32:25 +0000

    Revert "Fixed Routing"
    This reverts commit df59fe52f57f16e77e2b730cb8e65a07982fc2f0.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,15 +1,6 @@
 <?php
 
-Route::group([
-    'namespace' => 'Seat\Web\Http\Controllers',
-    'middleware' => 'web',   // Web middleware for state etc since L5.3
-], function () {
-
-    Route::group([
-        // 'namespace' => 'Stock',
-        // 'prefix' => 'stock',
-    ], function () {
-
-        include __DIR__ . '/Routes/Fuel.php';
-    });
-});
+// Fuel
+Route::get('/fuel')
+    ->name('seatcore::decoyFuel')
+    ->uses('FuelController@getFuel');
