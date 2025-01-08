dff46c0af9caf3b4737a479663bc49c9632e3dc3
BJKerridge <bjkerridge@live.co.uk>
2025-01-08 01:16:54 +0000

    Routing

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,15 +1,10 @@
 <?php
 
 Route::group([
-    'namespace' => 'Seat\Web\Http\Controllers',
-    'middleware' => 'web',   // Web middleware for state etc since L5.3
+    'namespace' => 'BJK\Decoy\Http\Controllers',
+    'middleware' => 'web',
 ], function () {
-
-    Route::group([
-        // 'namespace' => 'Stock',
-        // 'prefix' => 'stock',
-    ], function () {
-
+    Route::group([], function () {
         include __DIR__ . '/Routes/Fuel.php';
     });
 });
