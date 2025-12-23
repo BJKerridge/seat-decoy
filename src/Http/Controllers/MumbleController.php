5c0e1f0c434a93dd81e4a1284be52b406563799b
BJKerridge <bjkerridge@live.co.uk>
2025-12-23 19:20:24 +0000

    Remove Hardcoded Channel

--- src/Http/Controllers/MumbleController.php
+++ src/Http/Controllers/MumbleController.php
@@ -64,8 +64,7 @@ class MumbleController extends Controller
         $characterName = $main ? $main->name : 'Unknown';
         $characterId = $main ? $main->character_id : null;
         $passwordGenerator = Str::random(24); // 12-character random password
-        $discord_channel = env('DISCORD_NOTIFICATION_CHANNEL', 'poop');
         
-        return view('decoy::decoyMumble', compact('characterName', 'characterId', 'ticker', 'passwordGenerator', 'discord_channel'));
+        return view('decoy::decoyMumble', compact('characterName', 'characterId', 'ticker', 'passwordGenerator'));
     }    
 }
